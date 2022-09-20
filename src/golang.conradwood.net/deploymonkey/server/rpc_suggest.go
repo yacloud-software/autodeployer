799ebd7ace2ac31220d14434e892933780dc50f1
Conrad Wood <cnw@conradwood.net>
2022-09-20 16:34:01 +0100

    .
    Change-Id: I49d246bce6aadcf27b62bf5d61b4d59e7d06696a

---
+++ src/golang.conradwood.net/deploymonkey/server/rpc_suggest.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"golang.conradwood.net/apis/common"
+	pb "golang.conradwood.net/apis/deploymonkey"
+	"golang.conradwood.net/deploymonkey/config"
+	"golang.conradwood.net/deploymonkey/suggest"
+	"golang.conradwood.net/go-easyops/client"
+)
+
+func (d *DeployMonkey) GetSuggestions(ctx context.Context, req *pb.SuggestRequest) (*pb.SuggestionList, error) {
+	depl := pb.NewDeployMonkeyClient(client.Connect("deploymonkey.DeployMonkey"))
+	depls, err := d.GetDeploymentsFromCache(ctx, &common.Void{})
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get deployments from cache: %s", err)
+	}
+
+	cfg, err := config.GetConfig(depl)
+	if err != nil {
+		return nil, fmt.Errorf("Could not get config: %s", err)
+	}
+	s, err := suggest.Analyse(cfg, depls)
+	if err != nil {
+		return nil, fmt.Errorf("Suggestion failed: %s", err)
+	}
+	fmt.Println(s.String())
+	res := &pb.SuggestionList{}
+	for _, ac := range s.Starts {
+		sd := &pb.Suggestion{
+			Start: true,
+			Host:  ac.Host,
+			App:   ac.App,
+		}
+		res.Suggestions = append(res.Suggestions, sd)
+	}
+	for _, ac := range s.Stops {
+		sd := &pb.Suggestion{
+			Start: false,
+			Host:  ac.Host,
+			App:   ac.App,
+		}
+		res.Suggestions = append(res.Suggestions, sd)
+	}
+	return res, nil
+}
