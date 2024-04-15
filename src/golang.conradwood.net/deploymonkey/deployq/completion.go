1c72688a48bc66acec36cd866c0bd4f5c4103753
Conrad Wood <cnw@conradwood.net>
2024-04-15 10:04:51 +0100

    automatic commit
    Change-Id: I979fadd9aeec94785eec6f3fd8c0384d92bde54b

---
+++ src/golang.conradwood.net/deploymonkey/deployq/completion.go
@@ -0,0 +1,80 @@
+package deployq
+
+import (
+	"fmt"
+	ad "golang.conradwood.net/apis/autodeployer"
+	"time"
+)
+
+const (
+	GRACE_PERIOD_BEFORE_SHUT_DOWN = time.Duration(90) * time.Second
+)
+
+/*
+the second part. stuff has been deployed, now wait for it to perform well before shutting down older versions
+it picks up all transactions with the 'started' flag set
+*/
+func (q *DeployQueue) work_monitoring() {
+	t := time.Duration(5) * time.Second
+	for {
+		time.Sleep(t)
+
+		// find the transactions (with lock held)
+		var transactions []*deployTransaction
+		q.Lock()
+		for _, t := range q.requests {
+			if t.started {
+				transactions = append(transactions, t)
+			}
+		}
+		q.Unlock()
+
+		// deal with the transactions (without lock held)
+		for _, t := range transactions {
+			err := q.check_monitored(t)
+			if err != nil {
+				fmt.Printf("monitoring failed: %s\n", err)
+			}
+		}
+	}
+}
+func (q *DeployQueue) check_monitored(dt *deployTransaction) error {
+	var latest_ready time.Time
+	all_ready := true
+	for _, did := range dt.deployed_ids {
+		app := did.deployer.AppByID(did.ID)
+		if app == nil {
+			// transaction failed
+			// TODO: cleanup
+			dt.SetError(fmt.Errorf("new version failed unexpectedly on deployer %s", did.deployer.String()))
+			dt.sendUpdate(EVENT_FINISHED)
+			return nil
+		}
+		if app.Status == ad.DeploymentStatus_EXECUSER {
+			if !did.ready {
+				did.ready = true
+				did.ready_time = time.Now()
+			}
+		}
+		if did.ready && did.ready_time.After(latest_ready) {
+			latest_ready = did.ready_time
+		}
+		if !did.ready {
+			all_ready = false
+		}
+	}
+	if !all_ready {
+		// TODO: timeout here? (allow for 5 minutes or 10 minutes or something before assuming the instance
+		// will never become ready?)
+		fmt.Printf("DT %s has at least one instance that is not ready yet - no action yet\n", dt.String())
+		return nil
+	}
+	ago := time.Since(latest_ready)
+	if ago < GRACE_PERIOD_BEFORE_SHUT_DOWN {
+		fmt.Printf("DT %s started %0.1f seconds ago - no action yet\n", dt.String(), ago.Seconds())
+		return nil
+	}
+	// TODO: proceed to next step,
+	fmt.Printf("DT %s is now good\n", dt.String())
+	return nil
+}
